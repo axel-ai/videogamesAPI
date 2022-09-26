4fd6c65b7e8f15bd1e3cf7ce662582b268311568
axel-ai <melnikaxeel18@gmail.com>
2022-09-26 16:19:11 -0300

    s

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD || "xddd2333Q";
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_NAME = process.env.DB_NAME || "postgres";
 const API_KEY = process.env.API_KEY || "2c71615c7ac74394b941b0cc54585c54";
-const DB_PORT = process.env.PORT || 3001;
+const DB_PORT = process.env.DB_PORT || 3001;
 
 module.exports = {
   DB_USER,
