6dabb4527bf3f88a5f86a61d2d6559760ca6962d
axel-ai <melnikaxeel18@gmail.com>
2022-09-26 16:28:09 -0300

    ñ

--- config.js
+++ config.js
@@ -1,16 +1,21 @@
-const DB_USER = process.env.DB_USER || "postgres";
-const DB_PASSWORD = process.env.DB_PASSWORD || "xddd2333Q";
-const DB_HOST = process.env.DB_HOST || "localhost";
-const DB_NAME = process.env.DB_NAME || "postgres";
-const API_KEY = process.env.API_KEY || "2c71615c7ac74394b941b0cc54585c54";
-const DB_PORT = process.env.DB_PORT || 3001;
+const DB_USER = "postgres";
+const DB_PASSWORD = "w3d06bsY66k1Q5PO4sSu";
+const DB_HOST = "containers-us-west-90.railway.app";
+const DB_NAME = "railway";
+const API_KEY = "2c71615c7ac74394b941b0cc54585c54";
+const DB_PORT = 7679;
+// const DB_USER = process.env.DB_USER || "postgres";
+// const DB_PASSWORD = process.env.DB_PASSWORD || "xddd2333Q";
+// const DB_HOST = process.env.DB_HOST || "localhost";
+// const DB_NAME = process.env.DB_NAME || "postgres";
+// const API_KEY = process.env.API_KEY || "2c71615c7ac74394b941b0cc54585c54";
+// const DB_PORT = process.env.DB_PORT || 3001;
 
 module.exports = {
   DB_USER,
   DB_PASSWORD,
   DB_HOST,
   DB_NAME,
-  API_KEY,
   API_KEY,
   DB_PORT,
 };
