507dfba3c7ee1bbf20b9a41f40f30bd54f032ce1
axel-ai <melnikaxeel18@gmail.com>
2022-09-26 16:04:59 -0300

    otronuevo

--- index.js
+++ index.js
@@ -21,7 +21,6 @@
 const { DB_PORT } = require("./config.js");
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
